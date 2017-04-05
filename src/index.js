066d94d7bb9007286a9fd73b010ad25099aada30
Tyler Garlick <tjgarlick@gmail.com>
2017-04-05 14:15:37 -0600

    Added react-redux-firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import Thunk from 'redux-thunk';
 
 import './index.css';
 import Routes from './routes';
@@ -12,9 +14,25 @@ import Reducers from './reducers';
 const history = createHistory();
 const middleware = routerMiddleware(history);
 
+const config = {
+  apiKey: 'AIzaSyCiJwxNzY-6TFp_VIecqKxVAIqL_L2sjlA',
+  authDomain: 'classroom-tools-f7a09.firebaseapp.com',
+  databaseURL: 'https://classroom-tools-f7a09.firebaseio.com',
+  projectId: 'classroom-tools-f7a09',
+  storageBucket: 'classroom-tools-f7a09.appspot.com',
+  messagingSenderId: '643004676545'
+};
+
 const store = createStore(
   Reducers,
-  applyMiddleware(middleware)
+  {},
+  compose(
+    applyMiddleware(
+      Thunk.withExtraArgument(getFirebase)
+    ),
+    applyMiddleware(middleware),
+    reactReduxFirebase(config, { userProfile: 'users', enableLogging: true })
+  )
 );
 
 ReactDOM.render(
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
+import { firebaseStateReducer as firebase} from 'react-redux-firebase';
 
 import queue from './queue';
 
 export default combineReducers({
   queue,
-  router
+  router,
+  firebase
 });
